52f342012babe058e894b0f2190359775d933e6b
pranomostro <pranomestro@gmail.com>
2016-09-09 18:16:23 +0200

    Updated compiler flags.

--- config.mk
+++ config.mk
@@ -5,5 +5,5 @@ CC=cc
 AR=ar
 RANLIB=ranlib
 
-CFLAGS=-std=c99 -O3 -flto -Wall -pedantic -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -DGOOD_RAND
+CFLAGS=-std=c99 -O2 -Wall
 LDFLAGS=-s -static
